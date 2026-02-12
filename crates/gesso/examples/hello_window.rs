f7e3e9c12385d9ba7463a8e2cb54fa038afcb362
iamnbutler <iamnbutler@github.com>
2026-02-11 21:37:17 -0500

    Add hello_window example

---
+++ crates/gesso/examples/hello_window.rs
@@ -0,0 +1,46 @@
+//! Opens a window and runs until closed.
+
+use winit::{
+    application::ApplicationHandler,
+    event::WindowEvent,
+    event_loop::{ActiveEventLoop, ControlFlow, EventLoop},
+    window::{Window, WindowId},
+};
+
+#[derive(Default)]
+struct App {
+    window: Option<Window>,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        if self.window.is_none() {
+            let attrs = Window::default_attributes()
+                .with_title("Gesso - Hello Window")
+                .with_inner_size(winit::dpi::LogicalSize::new(800.0, 600.0));
+            self.window = Some(event_loop.create_window(attrs).unwrap());
+        }
+    }
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
+        match event {
+            WindowEvent::CloseRequested => {
+                event_loop.exit();
+            }
+            WindowEvent::RedrawRequested => {
+                // TODO: render here
+                if let Some(window) = &self.window {
+                    window.request_redraw();
+                }
+            }
+            _ => {}
+        }
+    }
+}
+
+fn main() {
+    let event_loop = EventLoop::new().unwrap();
+    event_loop.set_control_flow(ControlFlow::Wait);
+    let mut app = App::default();
+    event_loop.run_app(&mut app).unwrap();
+}
