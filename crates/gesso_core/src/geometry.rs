d04ea20b6676d17147f474c54a4efc9dc0b3618e
iamnbutler <iamnbutler@github.com>
2026-02-11 20:37:07 -0500

    Add Point, Size, Rect, Vector type aliases

--- crates/gesso_core/src/geometry.rs
+++ crates/gesso_core/src/geometry.rs
@@ -14,21 +14,19 @@ impl glamour::Unit for DevicePixels {
     type Scalar = f32;
 }
 
+// Logical space type aliases
+pub type Point = glamour::Point2<LogicalPixels>;
+pub type Size = glamour::Size2<LogicalPixels>;
+pub type Rect = glamour::Rect<LogicalPixels>;
+pub type Vector = glamour::Vector2<LogicalPixels>;
+
+// Device space type aliases
+pub type DevicePoint = glamour::Point2<DevicePixels>;
+pub type DeviceSize = glamour::Size2<DevicePixels>;
+pub type DeviceRect = glamour::Rect<DevicePixels>;
+
 #[cfg(test)]
 mod tests {
+    #[allow(unused_imports)]
     use super::*;
-    use glamour::Unit;
-
-    #[test]
-    fn logical_pixels_is_f32_unit() {
-        // LogicalPixels should implement Unit with f32 scalar
-        fn assert_unit<U: Unit<Scalar = f32>>() {}
-        assert_unit::<LogicalPixels>();
-    }
-
-    #[test]
-    fn device_pixels_is_f32_unit() {
-        fn assert_unit<U: Unit<Scalar = f32>>() {}
-        assert_unit::<DevicePixels>();
-    }
 }
